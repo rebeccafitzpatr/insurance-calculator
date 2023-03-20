4e0f5d5e484bafc74ca69d8d6614e2ff3b9cfe4d
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-03-20 14:01:22 +1300

    added arrayList to store profile information in createNewProfile method.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,6 +1,9 @@
 package nz.ac.auckland.se281;
 
+
 import nz.ac.auckland.se281.Main.PolicyType;
+
+import java.util.ArrayList;
 
 public class InsuranceSystem {
   public InsuranceSystem() {
@@ -12,12 +15,31 @@ public class InsuranceSystem {
 
     //System.out.println("Database has 0 profiles.");
     MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
-
+    
 
   }
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
+
+    
+    //* *//
+    //make an array list to store the profile information
+    //store each profiles information as a string separated by a; semicolon
+    ArrayList<String> profiles = new ArrayList<String>();
+    String profileEntry = userName + ";" + age;
+
+    //String profile = "profile";
+
+    //System.out.println(profileEntry);
+
+    
+    profiles.add(profileEntry);
+
+    //System.out.println(profiles);
+
+    //* */
+
   }
 
   public void loadProfile(String userName) {
