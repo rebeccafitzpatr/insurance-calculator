c17d3065cac52c8655818850e5ccd111783727d4
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-03-20 22:00:27 +1300

    fixed the build failure issue, and also added the logic for the 0 profiles output

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -3,11 +3,11 @@ package nz.ac.auckland.se281;
 
 import nz.ac.auckland.se281.Main.PolicyType;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class InsuranceSystem {  
 
-  //ArrayList<String> profiles = new ArrayList<String>();
+  ArrayList<String> profiles = new ArrayList<String>();
 
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
@@ -20,16 +20,21 @@ public class InsuranceSystem {
 
     //System.out.println("Database has 0 profiles.");
     
-    
-    //for (String profile :profiles) {
+    if (profiles.size() == 0){
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+    }
 
-      //String[] entries= profile.split(";");
+    for (String profile :profiles) {
+
+      String[] entries= profile.split(";");
 
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", ".", " ");
-      //MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage("%s: %s, %s", "1", entries[0], entries[1]);
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage("%s: %s, %s", "1", entries[0], entries[1]);
       
     }
-    //System.out.println(String userName);
+  }//System.out.println(String userName);
+
+
 
   
 
@@ -41,14 +46,14 @@ public class InsuranceSystem {
     //make an array list to store the profile information
     //store each profiles information as a string separated by a; semicolon
     //ArrayList<String> profiles = new ArrayList<String>();
-    //String profileEntry = userName + ";" + age;
+    String profileEntry = userName + ";" + age;
 
     //String profile = "profile";
 
     //System.out.println(profileEntry);
 
     
-    //profiles.add(profileEntry);
+    profiles.add(profileEntry);
 
     //System.out.println(profiles);
 
