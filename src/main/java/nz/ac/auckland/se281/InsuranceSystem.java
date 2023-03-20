1b3d42fc82a9b6306e12bdf908ff69049a6c8833
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-03-20 18:41:40 +1300

    there is a bug causing a build failure, unsure where to find it

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -3,21 +3,35 @@ package nz.ac.auckland.se281;
 
 import nz.ac.auckland.se281.Main.PolicyType;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
-public class InsuranceSystem {
+public class InsuranceSystem {  
+
+  //ArrayList<String> profiles = new ArrayList<String>();
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
+
+  //ArrayList<String> profiles = new ArrayList<String>();
   }
 
   public void printDatabase() {
     // TODO: Complete this method.
 
     //System.out.println("Database has 0 profiles.");
-    MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
     
+    
+    //for (String profile :profiles) {
 
-  }
+      //String[] entries= profile.split(";");
+
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", ".", " ");
+      //MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage("%s: %s, %s", "1", entries[0], entries[1]);
+      
+    }
+    //System.out.println(String userName);
+
+  
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
@@ -26,15 +40,15 @@ public class InsuranceSystem {
     //* *//
     //make an array list to store the profile information
     //store each profiles information as a string separated by a; semicolon
-    ArrayList<String> profiles = new ArrayList<String>();
-    String profileEntry = userName + ";" + age;
+    //ArrayList<String> profiles = new ArrayList<String>();
+    //String profileEntry = userName + ";" + age;
 
     //String profile = "profile";
 
     //System.out.println(profileEntry);
 
     
-    profiles.add(profileEntry);
+    //profiles.add(profileEntry);
 
     //System.out.println(profiles);
 
