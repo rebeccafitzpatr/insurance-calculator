c521c0502a5555a9dc945179d3b4f2bd9fe1327e
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-04-19 17:50:46 +1200

    continued on building the logic for creating a home policy, only started

--- src/main/java/nz/ac/auckland/se281/HomePolicy.java
+++ src/main/java/nz/ac/auckland/se281/HomePolicy.java
@@ -1,10 +1,16 @@
 package nz.ac.auckland.se281;
 
 public class HomePolicy extends Policy {
+    String address;
+    boolean rental;
 
-    public HomePolicy(int sumInsured) {
+    public HomePolicy(int sumInsured, String address, boolean rental) {
         super(sumInsured);
-        //TODO Auto-generated constructor stub
+
     }
     
+    public int HomeBasePremium(boolean rental){
+        //if the property is a rental, the base premium is 2% of the sum insured, if not it is 1% of the base premium.
+        return 0;
+    }
 }
